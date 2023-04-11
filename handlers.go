c6050acfd0332edf89b9e0dfeffcdc58589a3767
UmairEducative <115224931+UmairEducative@users.noreply.github.com>
2023-04-11 11:34:12 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -5,7 +5,153 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"encoding/csv"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
 )
+
+type Entry struct {
+	Name       string
+	Surname    string
+	Tel        string
+	LastAccess string
+}
+
+type PhoneBook []Entry
+
+var data = PhoneBook{}
+var index map[string]int
+
+func readCSVFile(filepath string) error {
+	_, err := os.Stat(filepath)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// CSV file read all at once
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return err
+	}
+
+	for _, line := range lines {
+		temp := Entry{
+			Name:       line[0],
+			Surname:    line[1],
+			Tel:        line[2],
+			LastAccess: line[3],
+		}
+		// Storing to global variable
+		data = append(data, temp)
+	}
+
+	return nil
+}
+
+func saveCSVFile(filepath string) error {
+	csvfile, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer csvfile.Close()
+
+	csvwriter := csv.NewWriter(csvfile)
+	for _, row := range data {
+		temp := []string{row.Name, row.Surname, row.Tel, row.LastAccess}
+		_ = csvwriter.Write(temp)
+	}
+	csvwriter.Flush()
+	return nil
+}
+
+func createIndex() error {
+	index = make(map[string]int)
+	for i, k := range data {
+		key := k.Tel
+		index[key] = i
+	}
+	return nil
+}
+
+// Initialized by the user – returns a pointer
+// If it returns nil, there was an error
+func initS(N, S, T string) *Entry {
+	// Both of them should have a value
+	if T == "" || S == "" {
+		return nil
+	}
+	// Give LastAccess a value
+	LastAccess := strconv.FormatInt(time.Now().Unix(), 10)
+	return &Entry{Name: N, Surname: S, Tel: T, LastAccess: LastAccess}
+}
+
+func insert(pS *Entry) error {
+	// If it already exists, do not add it
+	_, ok := index[(*pS).Tel]
+	if ok {
+		return fmt.Errorf("%s already exists", pS.Tel)
+	}
+
+	*&pS.LastAccess = strconv.FormatInt(time.Now().Unix(), 10)
+	data = append(data, *pS)
+	// Update the index
+	_ = createIndex()
+
+	err := saveCSVFile(CSVFILE)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteEntry(key string) error {
+	i, ok := index[key]
+	if !ok {
+		return fmt.Errorf("%s cannot be found!", key)
+	}
+	data = append(data[:i], data[i+1:]...)
+	// Update the index - key does not exist any more
+	delete(index, key)
+
+	err := saveCSVFile(CSVFILE)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func search(key string) *Entry {
+	i, ok := index[key]
+	if !ok {
+		return nil
+	}
+	data[i].LastAccess = strconv.FormatInt(time.Now().Unix(), 10)
+	return &data[i]
+}
+
+func matchTel(s string) bool {
+	t := []byte(s)
+	re := regexp.MustCompile(`\d+$`)
+	return re.Match(t)
+}
+
+func list() string {
+	var all string
+	for _, k := range data {
+		all = all + k.Name + " " + k.Surname + " " + k.Tel + "\n"
+	}
+	return all
+}
+
+// Handlers.go code starts from here:
 
 const PORT = ":1234"
 
